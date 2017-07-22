34040a77ba07968d291d62fc90165bd1b185fb2f
nandhiniu <30337570+nandhiniu@users.noreply.github.com>
2017-07-22 13:41:00 +0530

    j.c

---
+++ j.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the values",a);
+scanf("%d",&a);
+if(a<0)
+{
+printf("the value  is negative");
+}
+ else if(a=0)
+{
+printf("the value is zero");
+}
+else
+{
+printf("the value is positive");
+}
+return 0;
+}
